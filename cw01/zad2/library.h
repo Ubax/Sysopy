963fb74638b97b1b50f8b19ce88e77341c79c443
Jakub Tkacz <ubax7k@gmail.com>
2019-03-10 08:49:44 +0100

    New directories tree

---
+++ cw01/zad2/library.h
@@ -0,0 +1,20 @@
+#ifndef ZAD1LIB_LIBRARY_H
+#define ZAD1LIB_LIBRARY_H
+
+#include <stdio.h>
+
+struct Array{
+    char** block;
+    size_t size;
+};
+
+size_t fileSize(FILE *f);
+void findAndSaveResultToTemporaryFile(char *directory, char *fileName, char *tempFileName);
+char *readTemporaryFile(char *fileName);
+int createEmptyDefaultSizeArray(struct Array* array);
+int addTemporaryFileBlockPointerToArray(struct Array* array, char *tempFileName);
+void deleteBlockFromArray(struct Array* array, int i);
+void emptyArrayAndBlocks(struct Array* array);
+int createEmptyArray(struct Array* array, size_t size);
+
+#endif
