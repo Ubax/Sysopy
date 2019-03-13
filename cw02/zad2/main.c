16875bf11617f5e592c2a5af6b675b6559855db0
Jakub Tkacz <ubax7k@gmail.com>
2019-03-13 14:38:42 +0100

    time comparison not working

--- cw02/zad2/main.c
+++ cw02/zad2/main.c
@@ -4,14 +4,13 @@
 #include <time.h>
 #include <sys/times.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdarg.h>
 #include <errno.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 #define LOG 0
-#define HEADERS 1
+#define HEADERS 0
 
 static inline void my_log(const char *msg, ...) {
 #if LOG > 0
@@ -31,6 +30,101 @@ static inline void my_headers(const char *msg, ...) {
 #endif
 }
 
+enum ERRORS {
+    NO_ERROR = 0,
+    NO_DIRECTORY,
+    CLOSING_DIRECTORY,
+    READING_DIR,
+    FILE_INFO,
+};
+
+void displayError(char *prefix, enum ERRORS error) {
+    switch (error) {
+        case NO_ERROR:
+            break;
+        case NO_DIRECTORY:
+            printf("Error in %s: no directory\n", prefix);
+            break;
+        case CLOSING_DIRECTORY:
+            printf("Error in %s: closing directory\n", prefix);
+            break;
+        case READING_DIR:
+            printf("Error in %s: reading directory\n", prefix);
+            break;
+        case FILE_INFO:
+            printf("Error in %s: file info %i\n", prefix, errno);
+            break;
+    }
+}
+
+int comp(time_t time1, time_t time2, char *type) {
+    if (strcmp(type, ">") == 0)return time2 > time1;
+    if (strcmp(type, "=") == 0)return time2 == time1;
+    if (strcmp(type, "<") == 0)return time2 < time1;
+    return 0;
+}
+
+enum ERRORS ors(char *dir, char *type, char *date) {
+    my_headers("ORS\n");
+
+    if (dir[strlen(dir) - 1] != '/')strcat(dir, "/");
+
+    my_log("\tOpening directory...\n");
+    DIR *directory;
+    if ((directory = opendir(dir)) == NULL)return NO_DIRECTORY;
+
+    my_log("\tReading files...\n");
+    struct dirent *dirInfo;
+    struct stat fileInfo;
+    char dirName[4096];
+    while ((dirInfo = readdir(directory)) != NULL) {
+        if (strcmp(dirInfo->d_name, ".") == 0)continue;
+        else if (strcmp(dirInfo->d_name, "..") == 0)continue;
+
+        strcpy(dirName, dir);
+        strcat(dirName, dirInfo->d_name);
+        my_log("%s\n", dirName);
+        if (lstat(dirName, &fileInfo) == -1)return FILE_INFO;
+
+        if (S_ISDIR(fileInfo.st_mode) && !S_ISLNK(fileInfo.st_mode)) {
+            strcat(dirName, "/");
+            enum ERRORS error = ors(dirName, type, date);
+            if (error != NO_ERROR)return error;
+        } else {
+            if (comp(time(0) - 1000, fileInfo.st_mtim.tv_sec, type)) {
+                char date[256];
+                strftime(date, 255, "%F", localtime(&fileInfo.st_mtim.tv_sec));
+                printf("%s\t%s\n", dirInfo->d_name, date);
+            }
+        }
+    }
+
+    if (errno != 0)return READING_DIR;
+
+    my_log("\tClosing directory...\n");
+    if (closedir(directory) != 0)return CLOSING_DIRECTORY;
+    return NO_ERROR;
+}
+
+enum ERRORS n(char *dir, char *type, char *date) {
+    return NO_ERROR;
+}
+
+
+int ors_preprepare(char *dir, char *type, char *date) { // opendir, readdir, stat
+    enum ERRORS error = ors(dir, type, date);
+    displayError("ORS", error);
+    if (error != NO_ERROR)return 1;
+    return 0;
+}
+
+int n_preprepaer(char *dir, char *type, char *date) { //nftw
+    enum ERRORS error = n(dir, type, date);
+    displayError("NFTW", error);
+    if (error != NO_ERROR)return 1;
+    return 0;
+};
+
 int main(int argc, char **argv) {
     if (argc < 4) {
         printf("Program expects at 3 arguments: [directory] [type (<,=,>)] [date]\n");
@@ -40,7 +134,10 @@ int main(int argc, char **argv) {
         printf("Second argument should be one of < = >\n");
         return 1;
     }
-    printf("Correct\n");
+
+    if (ors_preprepare(argv[1], argv[2], argv[3]) != 0)return 1;
+
+    if (n_preprepaer(argv[1], argv[2], argv[3]) != 0)return 1;
     return 0;
 }
 
